177862f52df13498776ad8dc81c40cff4e138ead
plaschinsky <gplaschi@chicagobooth.edu>
2025-06-04 19:04:50 +0000

    4 dice rolls options

--- dice.rb
+++ dice.rb
@@ -5,6 +5,11 @@ require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
 
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 get("/") do
   "Hello World"
 end
@@ -27,10 +32,41 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 
-
-# Need this configuration for better_errors
-use(BetterErrors::Middleware)
-BetterErrors.application_root = __dir__
-BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
-
 end
+
+get("/dice/2/10") do
+  first_dice = rand (1..10)
+  second_dice = rand (1..10)
+  sum = first_dice + second_dice
+
+  outcome = "You rolled a #{first_dice} and a #{second_dice} for a total of #{sum}."
+  
+   "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+
+end 
+
+get("/dice/1/20") do
+  dice = rand (1..20)
+
+  outcome = "You rolled a #{dice}."
+  
+   "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+
+end 
+
+get("/dice/5/4") do
+  first_dice = rand (1..4)
+  second_dice = rand (1..4)
+  third_dice = rand (1..4)
+  fourth_dice = rand (1..4)
+  fifth_dice = rand (1..4)
+  sum = first_dice + second_dice + third_dice + fourth_dice + fifth_dice
+
+  outcome = "You rolled a #{first_dice}, #{second_dice}, #{third_dice}, #{fourth_dice} and #{fifth_dice} for a total of #{sum}."
+  
+   "<h1>5d4</h1>
+   <p>#{outcome}</p>"
+
+end 
